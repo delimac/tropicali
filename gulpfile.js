b659a6f2173b12c40148e7c83d6f4c2493b1913f
Chris De Lima <delimac@hey.com>
2021-08-05 12:42:28 +0800

    added css reset

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('default', function() {
+gulp.task("sass", function() {
   // place code for your default task here
+  // we need to run "sass css/app.scss app.css --watch"
+  return gulp.src("css/app.scss")
+    // pipe through sass compiler
+    .pipe(sass())
+    // where to save compiled css
+    .pipe(gulp.dest("."))
 });
+
+gulp.task("watch", function () {
+  gulp.watch("css/app.scss", ["sass"])
+})
+
+gulp.task('default', ["sass", "watch"]);
