caf0fe4523d6ef0bd8250a1677b71433a8490f2b
Chris De Lima <delimac@hey.com>
2021-08-05 13:16:28 +0800

    added gulp options and sourcemaps for css/scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
 var gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 var cleanCss = require("gulp-clean-css")
+var sourcemaps = require("gulp-sourcemaps")
 
 gulp.task("sass", function() {
   // place code for your default task here
   // we need to run "sass css/app.scss app.css --watch"
   return gulp.src("css/app.scss")
+    // initialize sourcemaps before starting sass
+    .pipe(sourcemaps.init())
     // pipe through sass compiler
     .pipe(sass())
     // clean up the css code
-    .pipe(cleanCss())
+    .pipe(
+      cleanCss({
+        compatibility: 'ie8'
+      })
+    )
+    // write sourcemaps
+    .pipe(sourcemaps.write())
     // where to save compiled css
     .pipe(gulp.dest("."))
 });
