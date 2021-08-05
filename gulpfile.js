d50b4a0a04d367d7697229ad58efb6c3321a6564
Chris De Lima <delimac@hey.com>
2021-08-05 13:03:14 +0800

    added minify css in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+var gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+var cleanCss = require("gulp-clean-css")
 
 gulp.task("sass", function() {
   // place code for your default task here
@@ -7,6 +8,8 @@ gulp.task("sass", function() {
   return gulp.src("css/app.scss")
     // pipe through sass compiler
     .pipe(sass())
+    // clean up the css code
+    .pipe(cleanCss())
     // where to save compiled css
     .pipe(gulp.dest("."))
 });
@@ -15,4 +18,4 @@ gulp.task("watch", function () {
   gulp.watch("css/app.scss", ["sass"])
 })
 
-gulp.task('default', ["sass", "watch"]);
+gulp.task('default', ["sass", "watch"])
